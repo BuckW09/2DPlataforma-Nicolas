37f412138d3d5d7e6ebf39a776448648688a7ed8
Nícolas <123389661+BuckW09@users.noreply.github.com>
2024-07-23 15:36:59 -0300

    ATUALIZACAO

--- My project/Assets/script/MecanicasBasica.cs
+++ My project/Assets/script/MecanicasBasica.cs
@@ -13,6 +13,10 @@ public class movbasica : MonoBehaviour
     public bool sensor;
 
     private Animator anim;
+    public bool verificarDirecaoPersonagem;
+    public GameObject municao;
+    public Transform posicTiro;
+    public float velocidadeTiro;
     void Start()
     {
         rbPlayer = GetComponent<Rigidbody2D>();
@@ -22,20 +26,48 @@ public class movbasica : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        verificarChao();
         movimentoHorizontal = Input.GetAxisRaw("Horizontal");
 
         rbPlayer.velocity = new Vector2(movimentoHorizontal * velocidade, rbPlayer.velocity.y);
+
+        if(movimentoHorizontal >0 && verificarDirecaoPersonagem == true){
+            Flip();
+        }
+        if (movimentoHorizontal < 0 && verificarDirecaoPersonagem == false)
+        {
+            Flip();
+        }
+
 
         if (Input.GetButtonDown("Jump") && sensor == true)
         {
             rbPlayer.AddForce(new Vector2(0, forcaPulo));
         }
+        if(Input.GetMouseButtonDown(0))
+        {
+            Atirar();
+        }
         anim.SetInteger("Run",(int)movimentoHorizontal);
+        anim.SetBool("sensor",sensor);
     }
 
 
     public void verificarChao()
     {
-        sensor = Physics2D.OverlapCircle(posicaoSensor.position, 0.34f);
+        sensor = Physics2D.OverlapCircle(posicaoSensor.position, 0.15f);
+    }
+
+    public void Flip()
+    {
+        verificarDirecaoPersonagem = !verificarDirecaoPersonagem;
+        float x = transform.localScale.x * -1;
+        transform.localScale = new Vector3(x, transform.localScale.y,transform.localScale.z);
+    }
+    public void Atirar()
+    {
+        GameObject temporario = Instantiate(municao);
+        temporario.transform.position = posicTiro.position;
+        temporario.GetComponent<Rigidbody2D>().velocity = new Vector2(velocidadeTiro, 0);
     }
 }
